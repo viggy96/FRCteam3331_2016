1fb7b197d53c70509b5e33ccc658e7b7e4c64f2a
vignesh <vigneshb.hp@gmail.com>
2016-01-25 16:13:18 -0500

    Initial commit.

---
+++ src/org/usfirst/frc/team3331/robot/RobotMap.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team3331.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    public static final Talon frontLeftMotor = new Talon(0);
+    public static final Talon frontRightMotor = new Talon(1);
+    public static final Talon rearLeftMotor = new Talon(2);
+    public static final Talon rearRightMotor = new Talon(3);
+    
+    public static final RobotDrive drivetrain = new RobotDrive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+    
+    public static final Joystick gamepad = new Joystick(0);
+    public static final int leftStickX = 0;
+    public static final int leftStickY = 1;
+    public static final int rightStickX = 4;
+    public static final int rightStickY = 5;
+}
---
+++ src/org/usfirst/frc/team3331/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team3331.robot.subsystems;
+
+import org.usfirst.frc.team3331.robot.RobotMap;
+import org.usfirst.frc.team3331.robot.commands.teleopDriveCommand;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveSubsystem extends Subsystem {
+    private static final int NORMALISE_POW = 2;
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+       setDefaultCommand(new teleopDriveCommand());
+    }
+    
+    public void init() {
+    	RobotMap.drivetrain.setLeftRightMotorOutputs(0, 0);
+    	RobotMap.drivetrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
+    	RobotMap.drivetrain.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+    }
+    
+    public void teleopDrive() {
+    	double axes[] = normaliseAxes();
+    	RobotMap.drivetrain.tankDrive(axes[0], axes[1]);
+    }
+    
+    private double[] normaliseAxes() {
+    	double left = Math.pow(RobotMap.gamepad.getRawAxis(RobotMap.leftStickY), NORMALISE_POW);
+    	double right = Math.pow(RobotMap.gamepad.getRawAxis(RobotMap.rightStickY), NORMALISE_POW);
+    	
+    	double output[] = {left, right};
+    	return output;
+    }
+}
+
